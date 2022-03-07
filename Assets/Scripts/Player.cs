168d2df7bf3bea37df54474fba121f663e85e4cd
Connor Turetzky <cturetzky@gmail.com>
2022-03-06 19:35:43 -0800

    Added character movement left & right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
   public GameObject bullet;
-
+  public float moveSpeed = 3.0f;
   public Transform shottingOffset;
     // Update is called once per frame
     void Update()
@@ -17,6 +17,14 @@ public class Player : MonoBehaviour
 
         Destroy(shot, 3f);
 
+      }
+
+      if (Input.GetKey("left")){
+        transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+      }
+
+      if (Input.GetKey("right")){
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
       }
     }
 }
