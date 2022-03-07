3d8b05281a9fc9b0725fc747018b146fe7105b75
Connor Turetzky <cturetzky@gmail.com>
2022-03-06 18:19:09 -0800

    Created prefabs for 4 different enemy types
    All of them use the same Enemy.cs script to determine the amount of points given on death

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+  public float moveTime;
+  public float shootCooldown;
+  public int enemyType;
+  private float timeSinceLastMove = 0;
+  private float timeSinceLastShot = 0;
+    void Update(){
+      timeSinceLastMove += Time.deltaTime;
+      timeSinceLastShot += Time.deltaTime;
+
+      if(timeSinceLastMove >= moveTime){
+        moveEnemies();
+        timeSinceLastMove = 0;
+      }
+
+      if (timeSinceLastShot >= shootCooldown && Random.Range(0, 10) == 1)
+      {
+        timeSinceLastShot = 0;
+        Debug.Log("Incoming!");
+
+      }
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
       Debug.Log("Ouch!");
+    }
+
+    void moveEnemies(){
+      Debug.Log("Moving enemies");
     }
 }
